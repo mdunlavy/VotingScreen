34dbae84d019e806a2931c977f4811d5a2da69e4
ARUIZV0322 <adrianrv0322@gmail.com>
2024-10-29 20:57:56 -0600

    Created Proposition class

---
+++ src/testSuite/Proposition.java
@@ -0,0 +1,45 @@
+package testSuite;
+
+public class Proposition {
+    private String name;
+    private String description;
+    private int maxNumSelections;// I think for simplicity we will only be choosing one option per proposition
+    private String[] options;
+    private boolean[] selections;
+
+    public Proposition(String name, String description, int maxNumSelections, String[] options) {
+        this.name = name;
+        this.description = description;
+        this.maxNumSelections = maxNumSelections;
+        this.options = options;
+        this.selections = new boolean[options.length];
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getMaxNumSelections() {
+        return this.maxNumSelections;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String[] getOptions() {
+        return this.options;
+    }
+
+    public boolean[] getSelections() {
+        return this.selections;
+    }
+
+    public void setSelection(int index, boolean newValue) {
+        try {
+            this.selections[index] = newValue;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: Index " + index + " is out of bounds for selections array.");
+        }
+    }
+}
